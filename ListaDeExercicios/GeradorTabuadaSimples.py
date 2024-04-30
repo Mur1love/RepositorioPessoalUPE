033be7a416785ea68007cc78d56e2d0a3ff3a8aa
Mur1love <murilormleal@outlook.com>
2024-04-30 14:02:45 -0300

    mudei para tabuada de soma

--- ListaDeExercicios/GeradorTabuadaSimples.py
+++ ListaDeExercicios/GeradorTabuadaSimples.py
@@ -1,15 +1,7 @@
 
-multiplicando = int(input('Digite um número para ver sua tabuada: '))
+adicionando = int(input('Digite um número para ver sua tabuada: '))
 
-print("A tabuada do", multiplicando, "é: ")
+print("A tabuada de soma do", adicionando, "é: ")
 
-print(multiplicando, 'x', '1 =', multiplicando * 1)
-print(multiplicando, 'x', '2 =', multiplicando * 2)
-print(multiplicando, 'x', '3 =', multiplicando * 3)
-print(multiplicando, 'x', '4 =', multiplicando * 4)
-print(multiplicando, 'x', '5 =', multiplicando * 5)
-print(multiplicando, 'x', '6 =', multiplicando * 6)
-print(multiplicando, 'x', '7 =', multiplicando * 7)
-print(multiplicando, 'x', '8 =', multiplicando * 8)
-print(multiplicando, 'x', '9 =', multiplicando * 9)
-print(multiplicando, 'x', '10 =', multiplicando * 10)
+for i in range(1 , 11): 
+    print(f"{adicionando} + {i} = {adicionando + i}")
